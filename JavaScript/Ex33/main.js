a9331420d7b9ee6dff70d7adc98be967adf2fcf9
K4-Khang <khang2145@gmail.com>
2024-01-10 19:18:55 +0700

    deleted

--- JavaScript/Ex33/main.js
+++
@@ -1,4 +0,0 @@
-let arr = [0,1,2,3,4,5,6]
-console.log(arr)
-arr = new Array()
-console.log(arr)
--- JavaScript/Ex35/main.js
+++
@@ -1,6 +0,0 @@
-
-function Random(item){
-    return item[Math.floor(Math.random()*item.length)]
-}
-var item = [1,2,3,4,5,6,7]
-console.log(Random(item))
